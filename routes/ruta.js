bb3cf20a8402b75e4ae06c1d6c0ea89fd2b15385
Monica <cmonikam@yahoo.es>
2021-02-17 14:46:24 -0300

    crear ruta borrar para botón comentario

--- routes/ruta.js
+++ routes/ruta.js
@@ -55,13 +55,24 @@ router.get('/', async (req, res) => {
 
 });
 
-router.get("/delete/:id", async (req, res) => { //ruta borrar
-  const borrar= await Message.findByPk(req.params.id);
-  await borrar.destroy();   
-  req.flash("aviso",`El mensaje ${borrar.message} fue eliminado con éxito`)
-  res.redirect("/")
+router.get("/delete/:id", async (req, res) => { 
 
+  const borrar= await Message.findByPk(req.params.id);
+  const borrar_o= await Comment.findByPk(req.params.id);
+  await borrar.destroy();
+  await borrar_o.destroy();  
+  req.flash("aviso",`El mensaje ${borrar.message} fue eliminado con éxito`)
+  req.flash("aviso",`El mensaje ${borrar_o.comment} fue eliminado con éxito`)
+  res.redirect("/")
 });
+
+router.get("/delete/comment/:id", async (req, res) => { 
+  const borrar_o= await Comment.findByPk(req.params.id);
+  await borrar_o.destroy();  
+ req.flash("aviso",`El mensaje ${borrar_o.comment} fue eliminado con éxito`)
+  res.redirect("/")
+});
+
 
 
 module.exports=router;
