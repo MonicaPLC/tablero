7c2572501aa080683d2fe1d76028ea06a7acb545
Monica <cmonikam@yahoo.es>
2021-02-17 14:31:43 -0300

    avances del ejercicio

--- routes/ruta.js
+++ routes/ruta.js
@@ -6,36 +6,59 @@ const {Message, Comment}=require('../db')
 router.post('/', async function(req, res){ 
  // al poner create ya se guarda No poner save.
  //await guarda en base de datos
+
+ if(req.body.author==""){
+  req.flash("aviso", 'nombre es obligatorio')
+  existeError=true;
+ }
+ if(req.body.message==""){
+  req.flash("aviso", 'debes escribir un mensaje')
+  existeError=true;
+ }
+ 
   const new_message= await Message.create({
     author: req.body.author,
     message: req.body.message,
   });
-  /*
-  const new_comentario= await Comment.create({
-    name: req.body.name,
-    comment: req.body.comment,
-  });
-  */
-
+  
   res.redirect("/"); 
     
   });
 
+  router.post('/comentarios', async function(req, res){ 
+    // al poner create ya se guarda No poner save.
+    //await guarda en base de datos
+     
+     const new_comentario= await Comment.create({
+       name: req.body.name,
+       comment: req.body.comment,
+       messageId:req.body.messageId,
+     });
+       
+     res.redirect("/"); 
+       
+     });
+
        
 //muestra mensajes capturados en la vista tablero
-router.get('/', async (req, res) => {  
-  const comentario = await Comment.findAll();
-  const mensaje = await Message.findAll();
-  res.render('tablero', {mensajes:mensaje, comentarios:comentario});
+router.get('/', async (req, res) => { 
+  const mensaje = await Message.findAll({
+    include:[Comment]
+  });
+  console.log(mensaje)
+  //const comentario = await Comment.findAll()
+  
+   let aviso=req.flash("aviso");
+
+  //console.log(aviso)
+  res.render('tablero', {mensajes:mensaje,aviso});
 
 });
 
 router.get("/delete/:id", async (req, res) => { //ruta borrar
   const borrar= await Message.findByPk(req.params.id);
-  const borrar_o= await Comment.findByPk(req.params.id);
-  await borrar.destroy(); 
-  res.redirect("/")
-  await borrar_o.destroy();
+  await borrar.destroy();   
+  req.flash("aviso",`El mensaje ${borrar.message} fue eliminado con éxito`)
   res.redirect("/")
 
 });
